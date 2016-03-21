d944c5eec7c8e3735644ce66292377195edd28c5
topameng <topameng@gmail.com>
2016-03-21 11:13:48 +0800

    1.0.4.80 版本更新

--- Assets/Editor/Custom/CustomSettings.cs
+++ Assets/Editor/Custom/CustomSettings.cs
@@ -10,7 +10,7 @@ public static class CustomSettings
     public static string saveDir = Application.dataPath + "/Source/Generate/";
     public static string luaDir = Application.dataPath + "/Lua/";
     public static string toluaBaseType = Application.dataPath + "/ToLua/BaseType/";
-
+    public static string toluaLuaDir = Application.dataPath + "/ToLua/Lua";
 
     //导出时强制做为静态类的类型(注意customTypeList 还要添加这个类型才能导出)
     //unity 有些类作为sealed class, 其实完全等价于静态类
@@ -30,8 +30,7 @@ public static class CustomSettings
     //附加导出委托类型(在导出委托时, customTypeList 中牵扯的委托类型都会导出， 无需写在这里)
     public static DelegateType[] customDelegateList = 
     {        
-        _DT(typeof(Action)),
-        //_DT(typeof(Action<GameObject>)).SetAbrName("ActionGo"),
+        _DT(typeof(Action)),        
         _DT(typeof(UnityEngine.Events.UnityAction)),       
         
         _DT(typeof(TestEventListener.OnClick)),
@@ -46,8 +45,8 @@ public static class CustomSettings
         //_GT(typeof(TestAccount)),
         //_GT(typeof(Dictionary<int, TestAccount>)).SetLibName("AccountMap"),                
         //_GT(typeof(KeyValuePair<int, TestAccount>)),    
-        //-------------------------------------------------------------------
-        
+        //-------------------------------------------------------------------        
+                                       
         _GT(typeof(Debugger)),                       
                                        
         _GT(typeof(Component)),
@@ -55,54 +54,81 @@ public static class CustomSettings
         _GT(typeof(MonoBehaviour)),        
         _GT(typeof(GameObject)),
         _GT(typeof(Transform)),
-        _GT(typeof(Space)),
-
-        _GT(typeof(Camera)),   
-        _GT(typeof(CameraClearFlags)),           
-        _GT(typeof(Material)),
-        _GT(typeof(Renderer)),        
-        _GT(typeof(MeshRenderer)),
-        _GT(typeof(SkinnedMeshRenderer)),
-        _GT(typeof(Light)),
-        _GT(typeof(LightType)),     
-        _GT(typeof(ParticleEmitter)),
-        _GT(typeof(ParticleRenderer)),
-        _GT(typeof(ParticleAnimator)),   
-        _GT(typeof(ParticleSystem)),                
+        _GT(typeof(TrackedReference)),
+        _GT(typeof(Application)),
         _GT(typeof(Physics)),
         _GT(typeof(Collider)),
+        _GT(typeof(Time)),        
+        _GT(typeof(Texture)),
+        _GT(typeof(Texture2D)),
+        _GT(typeof(Shader)),
+        _GT(typeof(Material)),
+        _GT(typeof(Renderer)),
+        _GT(typeof(WWW)),
+        _GT(typeof(Screen)),
+        _GT(typeof(Camera)),
+        _GT(typeof(CameraClearFlags)),
+        _GT(typeof(AudioClip)),
+        _GT(typeof(AudioSource)),
+        _GT(typeof(AssetBundle)),
+        _GT(typeof(ParticleSystem)),
+        _GT(typeof(AsyncOperation)).SetBaseType(typeof(System.Object)),
+        _GT(typeof(Light)),
+        _GT(typeof(LightType)),
+        _GT(typeof(SleepTimeout)),
+        _GT(typeof(Animator)),
+        _GT(typeof(Input)),
+        _GT(typeof(KeyCode)),
+        _GT(typeof(SkinnedMeshRenderer)),
+        _GT(typeof(Space)),        
+                                           
+        _GT(typeof(MeshRenderer)),            
+        _GT(typeof(ParticleEmitter)),
+        _GT(typeof(ParticleRenderer)),
+        _GT(typeof(ParticleAnimator)), 
+                              
         _GT(typeof(BoxCollider)),
         _GT(typeof(MeshCollider)),
         _GT(typeof(SphereCollider)),        
         _GT(typeof(CharacterController)),
-        _GT(typeof(Animation)),             
-        _GT(typeof(AnimationClip)),
-        _GT(typeof(TrackedReference)),
-        _GT(typeof(AnimationState)),  
-        _GT(typeof(QueueMode)),  
-        _GT(typeof(PlayMode)),                          
-        _GT(typeof(AudioClip)),
-        _GT(typeof(AudioSource)),                        
-        _GT(typeof(Application)),
-        _GT(typeof(Input)),              
-        _GT(typeof(KeyCode)),             
-        _GT(typeof(Screen)),
-        _GT(typeof(Time)),
-        _GT(typeof(RenderSettings)),
-        _GT(typeof(SleepTimeout)),                        
-        _GT(typeof(AsyncOperation)),
-        _GT(typeof(AssetBundle)),   
-        _GT(typeof(BlendWeights)),   
-        _GT(typeof(QualitySettings)),          
-        _GT(typeof(AnimationBlendMode)),  
-        _GT(typeof(RenderTexture)),
-        _GT(typeof(Rigidbody)), 
         _GT(typeof(CapsuleCollider)),
+        
+        _GT(typeof(Animation)),        
+        _GT(typeof(AnimationClip)).SetBaseType(typeof(UnityEngine.Object)),        
+        _GT(typeof(AnimationState)),
+        _GT(typeof(AnimationBlendMode)),
+        _GT(typeof(QueueMode)),  
+        _GT(typeof(PlayMode)),
         _GT(typeof(WrapMode)),
-        _GT(typeof(Texture)),
-        _GT(typeof(Shader)),
-        _GT(typeof(Texture2D)),
-        _GT(typeof(WWW)),
+
+        _GT(typeof(QualitySettings)),
+        _GT(typeof(RenderSettings)),                                                   
+        _GT(typeof(BlendWeights)),           
+        _GT(typeof(RenderTexture)),
+        _GT(typeof(Rigidbody)),         
+          
+    };
+
+    public static List<Type> dynamicList = new List<Type>()
+    {        
+        typeof(MeshRenderer),
+        typeof(ParticleEmitter),
+        typeof(ParticleRenderer),
+        typeof(ParticleAnimator),
+
+        typeof(BoxCollider),
+        typeof(MeshCollider),
+        typeof(SphereCollider),
+        typeof(CharacterController),
+        typeof(CapsuleCollider),
+
+        typeof(Animation),
+        typeof(AnimationClip),
+        typeof(AnimationState),        
+
+        typeof(BlendWeights),
+        typeof(RenderTexture),
+        typeof(Rigidbody),
     };
 
     //重载函数，相同参数个数，相同位置out参数匹配出问题时, 需要强制匹配解决
