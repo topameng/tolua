4cae9826913e793e76e78e5caa8fb982e8d3c033
chiguozi <qdlzr456@163.com>
2016-08-29 23:28:54 +0800

    修改panelbase

--- Assets/Lua/Logic/UI/PanelBase.lua
+++ Assets/Lua/Logic/UI/PanelBase.lua
@@ -5,6 +5,7 @@ local LoadState =
 	Init = 1,
 	Loading = 2,
 	Loaded = 3,
+	Unloaded = 4,
 }
 
 function PanelBase:initialize()
@@ -12,6 +13,22 @@ function PanelBase:initialize()
 	self.transform = nil
 	self.rectTransform = nil
 	self.isActive = false
+	self.state = LoadState.Init
+end
+
+function PanelBase:Show()
+	if self.isActive then
+		return
+	end
+	if self.state == LoadState.Loaded then
+		self:OnPreShow()
+		self:gameObject:SetActive(true)
+		self:OnShow()
+	else
+end
+
+
+function PanelBase:Hide()
 end
 
 function PanelBase:Init()
@@ -20,13 +37,33 @@ end
 function PanelBase:InitUI()
 end
 
+function PanelBase:OnPreShow()
+end
+
+function PanelBase:OnShow()
+end
 
 
 
 function PanelBase:FindChild(path)
 	if nil == self.transform then
-		return
+		return nil
 	end
 	return self.transform:FindChild(path)
 end
 
+function PanelBase:GetComponent(path, type)
+	local tr = self.FindChild(path)
+	if tr == nil then
+		return nil
+	end
+	return tr.gameObject:GetComponent(type)
+end
+
+function PanelBase:AddComponet(path, type)
+	local tr = self.FindChild(path)
+	if tr == nil then
+		return nil
+	end
+	tr.gameObject:AddComponet(type)
+end
