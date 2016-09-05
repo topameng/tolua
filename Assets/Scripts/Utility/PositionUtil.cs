f714ef4df05810aba3bdcd27f0f16377a254d3ab
chiguozi <qdlzr456@163.com>
2016-09-05 21:32:04 +0800

    positionUitl

---
+++ Assets/Scripts/Utility/PositionUtil.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PositionUtil
+{
+    public static Vector3 WorldPos2UIPos(Vector3 pos)
+    {
+        var cam = CameraControl.curCamera;
+        if (nill == cam)
+            return Vector3.zero;
+        Vector3 screenPos = cam.WorldToScreenPoint(pos);
+        var uiPos = ScreenPos2UIPos(screenPos);
+        return uiPos;
+    }
+
+    public static Vector3 WorldPos2ScreenPos(Vector3 pos)
+    {
+        var cam = CameraControl.curCamera;
+        if (nill == cam)
+            return Vector3.zero;
+        Vector3 screenPos = cam.WorldToScreenPoint(pos);
+        return screenPos;
+    }
+
+
+    public static Vector2 ScreenPos2UIPos(Vector2 pos)
+    {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+        pos.x = (pos.x / screenWidth) * Define.UIWIDTH;
+        pos.y = (pos.y / screenHeight) * Define.UIHEIGHT;
+        return pos;
+    }
+
+    public static bool CheckInScreen(Vector3 worldPos, float offsetX, float offsetY)
+    {
+        Vector2 uiPos = WorldPos2UIPos(worldPos);
+        if(uiPos.x < -offsetX || uiPos.y < -offsetY
+            || uiPos.x < Define.UIWIDTH + offsetX
+            || uiPos.y < Define.UIHEIGHT + offsetY)
+            return false;
+        return true;
+
+    }
+}
