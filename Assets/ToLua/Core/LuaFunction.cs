4580366c367025a885eec68b2634ab341876cdb3
topameng <topameng@gmail.com>
2016-12-03 17:18:35 +0800

    1.0.6.276 push byte[] 可定制长度

--- Assets/ToLua/Core/LuaFunction.cs
+++ Assets/ToLua/Core/LuaFunction.cs
@@ -353,11 +353,16 @@ namespace LuaInterface
             luaState.PushArgs(args);
         }
 
-        public void PushByteBuffer(byte[] buffer)
+        public void PushByteBuffer(byte[] buffer, int len = -1)
         {
             try
             {
-                luaState.PushByteBuffer(buffer);
+                if (len == -1)
+                {
+                    len = buffer.Length;
+                }
+
+                luaState.PushByteBuffer(buffer, len);
                 ++argCount;
             }
             catch (Exception e)
