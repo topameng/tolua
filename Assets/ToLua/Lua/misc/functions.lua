333ef4d24293de040a16d98ae7f7acff18403b6c
toopameng <topameng@gmail.com>
2017-02-06 12:28:29 +0800

    1.0.6.286 add string.split function

--- Assets/ToLua/Lua/misc/functions.lua
+++ Assets/ToLua/Lua/misc/functions.lua
@@ -2,6 +2,20 @@ local require = require
 local string = string
 local table = table
 
+function string.split(input, delimiter)
+    input = tostring(input)
+    delimiter = tostring(delimiter)
+    if (delimiter=='') then return false end
+    local pos,arr = 0, {}
+    -- for each divider found
+    for st,sp in function() return string.find(input, delimiter, pos, true) end do
+        table.insert(arr, string.sub(input, pos, st - 1))
+        pos = sp + 1
+    end
+    table.insert(arr, string.sub(input, pos))
+    return arr
+end
+
 function import(moduleName, currentModuleName)
     local currentModuleNameParts
     local moduleFullName = moduleName
