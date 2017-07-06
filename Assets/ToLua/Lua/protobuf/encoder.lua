b5f8e3807c457be884786192c4ea764d53b15c08
topameng <topameng@gmail.com>
2017-07-06 21:21:34 +0800

    1.0.7.327 修复pb嵌套协议中64位size计算Bug

--- Assets/ToLua/Lua/protobuf/encoder.lua
+++ Assets/ToLua/Lua/protobuf/encoder.lua
@@ -19,26 +19,40 @@ local string = string
 local table = table
 local ipairs = ipairs
 local assert =assert
+local tonumber = tonumber
 
 local pb = require "pb"
 local wire_format = require "protobuf.wire_format"
 module "protobuf.encoder"
 
+--临时
 function _VarintSize(value)
+    value = tonumber(value)
     if value <= 0x7f then return 1 end
     if value <= 0x3fff then return 2 end
     if value <= 0x1fffff then return 3 end
     if value <= 0xfffffff then return 4 end
-    return 5 
+    if value <= 0x7ffffffff then return 5 end
+    if value <= 0x3ffffffffff then return 6 end
+    if value <= 0x1ffffffffffff then return 7 end
+    if value <= 0xffffffffffffff then return 8 end
+    if value <= 0x7fffffffffffffff then return 9 end
+    return 10
 end
 
 function _SignedVarintSize(value)
+    value = tonumber(value)
     if value < 0 then return 10 end
     if value <= 0x7f then return 1 end
     if value <= 0x3fff then return 2 end
     if value <= 0x1fffff then return 3 end
     if value <= 0xfffffff then return 4 end
-    return 5
+    if value <= 0x7ffffffff then return 5 end
+    if value <= 0x3ffffffffff then return 6 end
+    if value <= 0x1ffffffffffff then return 7 end
+    if value <= 0xffffffffffffff then return 8 end
+    if value <= 0x7fffffffffffffff then return 9 end
+    return 10
 end
 
 function _TagSize(field_number)
