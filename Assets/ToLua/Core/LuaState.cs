fd9b9174ae5b486e5e1920d6b1a5eefc5aa5bdbe
NewbieGameCoder <596809147@qq.com>
2018-08-04 00:03:30 +0800

    自动剥离已Destroy的，被lua强引用的GameObject等重载了null判断的Unity资源，防止内存泄露过多

--- Assets/ToLua/Core/LuaState.cs
+++ Assets/ToLua/Core/LuaState.cs
@@ -1710,6 +1710,7 @@ namespace LuaInterface
 
         public int Collect()
         {
+            translator.StepCollect();
             int count = gcList.Count;
 
             if (count > 0)
--- Assets/ToLua/Core/ObjectTranslator.cs
+++ Assets/ToLua/Core/ObjectTranslator.cs
@@ -59,6 +59,7 @@ namespace LuaInterface
         public readonly Dictionary<object, int> objectsBackMap = new Dictionary<object, int>(new CompareObject());
         public readonly LuaObjectPool objects = new LuaObjectPool();
         private List<DelayGC> gcList = new List<DelayGC>();
+        private Action<object> removeDestroyedGameObject;
 
 #if !MULTI_STATE
         private static ObjectTranslator _translator = null;
@@ -70,6 +71,7 @@ namespace LuaInterface
 #if !MULTI_STATE
             _translator = this;
 #endif
+            removeDestroyedGameObject = (obj) => { objectsBackMap.Remove(obj); };
         }
 
         public int AddObject(object obj)
@@ -231,6 +233,11 @@ namespace LuaInterface
                     gcList[i].time = time;
                 }
             }
+        }
+
+        public void StepCollect()
+        {
+            objects.StepCollect(removeDestroyedGameObject);
         }
 
         public void Dispose()
